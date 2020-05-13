c2c5de2e19a82f44bdca5dd0a949850a74a91e48
pemachele <pemachele96@gmail.com>
2020-05-14 02:28:07 +0300

    Updates

---
+++ precache-manifest.209cd93c83f2fc18a013f33d24abf9a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89493592ac2140054297156126144c92",
+    "url": "/covid19-_EA/index.html"
+  },
+  {
+    "revision": "be11a2938efac643e650",
+    "url": "/covid19-_EA/static/css/main.8bf1bf04.chunk.css"
+  },
+  {
+    "revision": "823e14179b18be567f3c",
+    "url": "/covid19-_EA/static/js/2.ef580980.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-_EA/static/js/2.ef580980.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be11a2938efac643e650",
+    "url": "/covid19-_EA/static/js/main.6b945675.chunk.js"
+  },
+  {
+    "revision": "61f2fcfc65d6836dc4de",
+    "url": "/covid19-_EA/static/js/runtime-main.400a653a.js"
+  },
+  {
+    "revision": "179aa7e3545675c43ae12909ebcf1547",
+    "url": "/covid19-_EA/static/media/covid-19.179aa7e3.jpg"
+  }
+]);
